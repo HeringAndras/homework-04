b12bf1f854ace74a9a4eb040ec5be3645c7a0ffd
HeringAndras <hering.bandi@gmail.com>
2017-04-29 16:28:15 +0200

    3/1

--- src/homework-04.R
+++ src/homework-04.R
@@ -10,6 +10,7 @@ if (!("ggplot2" %in% installed.packages())) {
 
 library(ggplot2)
 
+
 #### 2/1 ####
 
 tweets <- read.csv(file = "~/homework-04/data/clinton_trump_tweets.csv",
@@ -17,12 +18,12 @@ tweets <- read.csv(file = "~/homework-04/data/clinton_trump_tweets.csv",
 
 #### 2/2 ####
 
-tweetnumber<-as.data.frame(table(tweets$handle))
+table(tweets$handle)
 
 demrepcol <- c("#232066" , "#E91D0E")
 
-ggplot(tweetnumber,aes(Var1,Freq,fill = Var1)) + 
-  geom_bar(stat = "identity") +
+ggplot(tweets,aes(handle,fill = handle)) + 
+  geom_bar(stat = "count") +
   ggtitle("Candidate Tweets") +
   theme(plot.title = element_text(hjust = 0.5),
         panel.background = element_rect(fill = "white", colour = NULL)) +
@@ -57,7 +58,13 @@ tweets$lang[tweets$lang == "es" & tweets$handle == "realDonaldTrump"] <- "en"
 
 langhaszn <- as.data.frame(table(tweets$handle,tweets$lang))
 
-langhaszn <- subset(langhaszn,( langhaszn$Var2 == "en") | (langhaszn$Var2 == "es"))
+langhaszn <- subset(langhaszn,(langhaszn$Var2 == "en")|(langhaszn$Var2 == "es"))
+
+#külön változóban kell használni a tweetek számát, mert a position dodge-ot
+#nem lehet rábírni arra, hogy Trump oszlopát ne szélesítse ki végig.
+#a http://ggplot2.tidyverse.org szerint van egy olyan, hogy preserve = "single"
+# argumentum a position_dodge-ban , de itt nem akar működni, és a helpben sincs
+# ilyenről szó. Minden up-to-date.
 
 palette2 <- c("darkgrey","cornflowerblue")
 
@@ -74,7 +81,81 @@ ggsave("fig/tweet2.png")
 
 #### 2/4 ####
 
+source("~/homework-04/src/homework-04-functions.R")
+
 tweetek_csokkeno("Hillary Clinton",10)
 tweetek_csokkeno("Donald Trump",15)
 tweetek_csokkeno("kalap")
 tweetek_csokkeno()
+
+#### 3/1 ####
+
+if (!("fivethirtyeight" %in% installed.packages())) {
+  install.packages("fivethirtyeight")
+}
+
+library(fivethirtyeight)
+data("hiphop_cand_lyrics")
+
+View(hiphop_cand_lyrics)
+
+
+ggplot(hiphop_cand_lyrics,
+       aes(album_release_date, fill = candidate)) +
+  geom_bar(stat = "count", position = position_stack())
+
+# képtelen egymásra rakni a stackeket, itt is megint kerülőútra van szükség
+
+hiphiphurra <- as.data.frame(table(hiphop_cand_lyrics$candidate,
+                                   hiphop_cand_lyrics$album_release_date))
+
+ggplot(hiphiphurra,aes(Var2,Freq, fill = Var1)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  xlab("") +
+  ylab("") +
+  ggtitle("Every mention of 2016 primary candidates in hip-hop songs") +
+  theme(plot.title = element_text(hjust = 0),
+        panel.grid.major = element_line(colour = "darkgrey"),
+        panel.grid.minor = element_line(colour = "darkgrey"),
+        panel.background = element_rect(fill = "lightgrey",colour = "lightgrey"),
+        plot.background = element_rect(fill = "lightgrey"),
+        legend.position = "top",
+        legend.background = element_rect(fill ="lightgrey")
+        ) +
+  scale_x_discrete(labels = c("1990" = "1990", "1995" = "'95","2000" = "2000",
+                              "2005" = "'05","2010" = "'10","2015" = "'15"),
+                   breaks = c(1990,1995,2000,2005,2010,2015))
+
+ggsave("fig/hiphop1.png")
+
+hiphiphurra2 <- as.data.frame(table(hiphop_cand_lyrics$candidate,
+                                    hiphop_cand_lyrics$album_release_date,
+                                    hiphop_cand_lyrics$sentiment))
+hiphiphurra2 <- hiphiphurra2[hiphiphurra2$Var3 == "positive",]
+
+ggplot(hiphiphurra2,aes(Var2,Freq, fill = Var1)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  xlab("") +
+  ylab("") +
+  ggtitle("Positive") +
+  theme(plot.title = element_text(hjust = 0.5),
+        panel.grid.major = element_line(colour = "darkgrey"),
+        panel.grid.minor = element_line(colour = "darkgrey"),
+        panel.background = element_rect(fill = "lightgrey",colour = "lightgrey"),
+        plot.background = element_rect(fill = "lightgrey"),
+        legend.position = "top",
+        legend.background = element_rect(fill ="lightgrey")
+  ) +
+  scale_x_discrete(labels = c("1990" = "1990", "1995" = "'95","2000" = "2000",
+                              "2005" = "'05","2010" = "'10","2015" = "'15"),
+                   breaks = c(1990,1995,2000,2005,2010,2015))
+
+ggsave("fig/hiphop2.png")
+
+
+#### 3/2 #### 
+
+
+#### 4/1 ####
+
+
